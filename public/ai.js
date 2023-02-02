59b6d2bb1b9b3273fdbd0cdfde7a60d76d555034
bornin2k4 <84788741+bornin2k4@users.noreply.github.com>
2023-02-02 19:14:32 +0530

    Update ai.js

--- public/ai.js
+++ public/ai.js
@@ -27,8 +27,14 @@ function speakNow() {
   recognition.start();
 }
 
+var i = 0;
+if (i==0) {
+  i+=1;
+  speakOutLoud("...");
+}
 
 function speakOutLoud(transcript) {
+
   let spoken = transcript;
 
   // The action of saying or expressing something aloud..
@@ -37,49 +43,47 @@ function speakOutLoud(transcript) {
 
   const utterance = new SpeechSynthesisUtterance();
 
-
-
   // Artificial production of human speech
   const speechSynthesis =
     window.speechSynthesis || window.webkitspeechSynthesis;
+
+  var voices = window.speechSynthesis.getVoices();
+  utterance.voice = voices[2]; // Note: some voices don't support altering params
+
+  utterance.voiceURI = 'native';
+  utterance.volume = 1; // 0 to 1
+  utterance.rate = 1; // 0.1 to 10
+  utterance.pitch = 1; //0 to 2
+  utterance.lang = 'en-US';
 
   // Thalha - 22/01/23 
   // Greeting 1
   var greeting1 = ["hi", "hello", "hai"];
   for (item of greeting1) {
     if (spoken.includes(item)) {
-      utterance.volume = 1; 
-      utterance.rate = 1;
-      utterance.pitch = 1;
-      utterance.text = "Hi, It worked";
+      utterance.text = "Hello...";
       speechSynthesis.speak(utterance);
       break;
     }
   }
-  
+
   // Arunachalam - 23/01/23 
   // Greetings2
-  var greeting2 = ["how are you", "how about you","how are you doing","is all well"];
+  var greeting2 = ["how are you", "how about you", "how are you doing", "is all well"];
   for (item of greeting2) {
     if (spoken.includes(item)) {
-      utterance.volume = 1; 
-      utterance.rate = 1;
-      utterance.pitch = 1;
       utterance.text = "Thanks For Asking, I'm Fine...";
       speechSynthesis.speak(utterance);
       break;
     }
   }
-  
+
   // Thalha - 24/01/23
   // service
   var service = ["what are the services provided"];
   for (item of service) {
     if (spoken.includes(item)) {
-      utterance.volume = 1; 
-      utterance.rate = 1;
-      utterance.pitch = 1;
-      utterance.text = "We Provide all Web related services like Website Designing, Development...";
+      utterance.text = "We Provide all Web related services from Web Designing to Development...";
       speechSynthesis.speak(utterance);
       break;
     }
@@ -89,3 +93,4 @@ function speakOutLoud(transcript) {
 
 
 }
+
